754373727c1e1186560d83784c33d72e020f7083
Giovanni Santos <giovannibarros_99@hotmail.com>
2023-09-04 13:06:57 -0300

    salvar conteudo da api em arquivo

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import requests as req
+import json
 
 # formato requerido URL : https://re.jrc.ec.europa.eu/api/v5_2/DRcalc?lat=-1.0714&lon=-48.1264&month=9&global=1&raddatabase=PVGIS-SARAH2&localtime=1&angle=10&aspect=180&showtemperatures=1&outputformat=json
 
@@ -20,8 +21,15 @@ api_params = {
 }
 
 try:
-    res = req.get(api_url,params=api_params, headers=api_headers)
+    res = req.get(api_url, params=api_params, headers=api_headers)
     res.raise_for_status()
     json_res = res.json()
+
+    try:
+        with open ("sample.json", "w") as out_file:
+            data = json.dump(json_res, out_file)
+    except Exception as e:
+        print(f"Erro ao processar JSON: {e}")
+
 except req.exceptions.RequestException as e:
     raise SystemExit(e)
